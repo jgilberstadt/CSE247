5e10c48ab423d46747366188c68535f98320fbcc
joshgilberstadt <joshgilberstadt@joshs-mbp.dhcp.wustl.edu>
2019-04-24 19:33:41 -0500

    Commit

--- labs/heaps/MinHeap.java
+++ labs/heaps/MinHeap.java
@@ -16,15 +16,15 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 	private final Ticker ticker;
 
 	/**
-	 * I've implemented this for you.  We create an array
-	 *   with sufficient space to accommodate maxSize elements.
-	 *   Remember that we are not using element 0, so the array has
-	 *   to be one larger than usual.
+	 * I've implemented this for you. We create an array with sufficient space to
+	 * accommodate maxSize elements. Remember that we are not using element 0, so
+	 * the array has to be one larger than usual.
+	 * 
 	 * @param maxSize
 	 */
 	@SuppressWarnings("unchecked")
 	public MinHeap(int maxSize, Ticker ticker) {
-		this.array = new Decreaser[maxSize+1];
+		this.array = new Decreaser[maxSize + 1];
 		this.size = 0;
 		this.ticker = ticker;
 	}
@@ -32,37 +32,36 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 	//
 	// Here begin the methods described in lecture
 	//
-	
+
 	/**
-	 * Insert a new thing into the heap.  As discussed in lecture, it
-	 *   belongs at the end of objects already in the array.  You can avoid
-	 *   doing work in this method by observing, as in lecture, that
-	 *   inserting into the heap is reducible to calling decrease on the
-	 *   newly inserted element.
-	 *   
-	 *   This method returns a Decreaser instance, which for the inserted
-	 *   thing, tracks the thing itself, the location where the thing lives
-	 *   in the heap array, and a reference back to MinHeap so it can call
-	 *   decrease(int loc) when necessary.
+	 * Insert a new thing into the heap. As discussed in lecture, it belongs at the
+	 * end of objects already in the array. You can avoid doing work in this method
+	 * by observing, as in lecture, that inserting into the heap is reducible to
+	 * calling decrease on the newly inserted element.
+	 * 
+	 * This method returns a Decreaser instance, which for the inserted thing,
+	 * tracks the thing itself, the location where the thing lives in the heap
+	 * array, and a reference back to MinHeap so it can call decrease(int loc) when
+	 * necessary.
 	 */
 	public Decreaser<T> insert(T thing) {
 		//
 		// Below we create the "handle" through which the value of
-		//    the contained item can be decreased.
+		// the contained item can be decreased.
 		// VERY IMPORTANT!
-		//    The Decreaser object contains the current location
-		//    of the item in the heap array.  Initially it's ++size,
-		//    as shown below.  The size is increased by 1, and that's
-		//    were you should store ans in the heap array.
+		// The Decreaser object contains the current location
+		// of the item in the heap array. Initially it's ++size,
+		// as shown below. The size is increased by 1, and that's
+		// were you should store ans in the heap array.
 		//
-		//    If and when the element there changes location in the heap
-		//    array, the .loc field of the Decreaser must change to reflect
-		//    that.
+		// If and when the element there changes location in the heap
+		// array, the .loc field of the Decreaser must change to reflect
+		// that.
 		//
 		Decreaser<T> ans = new Decreaser<T>(thing, this, ++size);
 		//
 		// You have to now put ans into the heap array
-		//   Recall in class we reduced insert to decrease
+		// Recall in class we reduced insert to decrease
 		//
 		// FIXME
 		//
@@ -71,64 +70,59 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 		decrease(size);
 		ticker.tick(5);
 		return ans;
+
 	}
 
 	/**
-	 * This method responds to an element in the heap decreasing in
-	 * value.   As described in lecture, that element might have to swap
-	 * its way up the tree so that the heap property is maintained.
+	 * This method responds to an element in the heap decreasing in value. As
+	 * described in lecture, that element might have to swap its way up the tree so
+	 * that the heap property is maintained.
 	 * 
-	 * This method can be called from within this class, in response
-	 *   to an insert.  Or it can be called from a Decreaser.
-	 *   The information needed to call this method is the current location
-	 *   of the heap element (index into the array) whose value has decreased.
-	 *   
-	 * Really important!   If this method changes the location of elements in
-	 *   the array, then the loc field within those elements must be modified 
-	 *   too.  For example, if a Decreaser d is currently at location 100,
-	 *   then d.loc == 100.  If this method moves that element d to
-	 *   location 50, then this method must set d.loc = 50.
-	 *   
+	 * This method can be called from within this class, in response to an insert.
+	 * Or it can be called from a Decreaser. The information needed to call this
+	 * method is the current location of the heap element (index into the array)
+	 * whose value has decreased.
+	 * 
+	 * Really important! If this method changes the location of elements in the
+	 * array, then the loc field within those elements must be modified too. For
+	 * example, if a Decreaser d is currently at location 100, then d.loc == 100. If
+	 * this method moves that element d to location 50, then this method must set
+	 * d.loc = 50.
+	 * 
 	 * In my solution, I made sure the above happens by writing a method
-	 *    moveItem(int from, int to)
-	 * which moves the Decreaser from index "from" to index "to" and, when
-	 * done, sets array[to].loc = to
-	 *   
-	 * This method is missing the "public" keyword so that it
-	 *   is only callable within this package.
-	 * @param loc position in the array where the element has been
-	 *     decreased in value
+	 * moveItem(int from, int to) which moves the Decreaser from index "from" to
+	 * index "to" and, when done, sets array[to].loc = to
+	 * 
+	 * This method is missing the "public" keyword so that it is only callable
+	 * within this package.
+	 * 
+	 * @param loc position in the array where the element has been decreased in
+	 *            value
 	 */
 	void decrease(int loc) {
 		//
 		// As described in lecture
 		//
-		if (loc==1) {
-		ticker.tick(2);
-		return;
-		}else {
-		if ((array[loc].getValue().compareTo(array[loc/2].getValue()))<0) {
-		moveItem(loc, loc/2);
-		ticker.tick(2);
+		if (loc <= 1) {
+			ticker.tick();
+			return;
 		}
+		if (array[loc].getValue().compareTo(array[loc / 2].getValue()) < 0) {
+			Decreaser<T> dec = array[loc];
+			array[loc] = array[loc / 2];
+			array[loc / 2] = dec;
+			array[loc].loc = loc;
+			array[loc / 2].loc = loc / 2;
+			decrease(loc / 2);
+			ticker.tick(6);
 		}
 	}
-	
-	void moveItem(int from, int to) {
-	Decreaser<T> x = array[from];
-	array[from] = array[to];
-	array[to] = x;
-	array[to].loc = to;
-	array[from].loc = from;
-	ticker.tick(5);
-	}
-	
+
 	/**
-	 * Described in lecture, this method will return a minimum element from
-	 *    the heap.  The hole that is created is handled as described in
-	 *    lecture.
-	 *    This method should call heapify to make sure the heap property is
-	 *    maintained at the root node (index 1 into the array).
+	 * Described in lecture, this method will return a minimum element from the
+	 * heap. The hole that is created is handled as described in lecture. This
+	 * method should call heapify to make sure the heap property is maintained at
+	 * the root node (index 1 into the array).
 	 */
 	public T extractMin() {
 		//
@@ -141,93 +135,108 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 		//
 		T ans = array[1].getValue();
 		array[1] = array[size];
+		array[1].loc=1;
 		array[size] = null;
 		--this.size;
-		array[1].loc=1;
-		if (size > 1) {
 		heapify(1);
-		}
 		ticker.tick(7);
 		return ans;
 	}
 
+
 	/**
-	 * As described in lecture, this method looks at a parent and its two 
-	 *   children, imposing the heap property on them by perhaps swapping
-	 *   the parent with the lesser of the two children.  The child thus
-	 *   affected must be heapified itself by a recursive call.
+	 * As described in lecture, this method looks at a parent and its two children,
+	 * imposing the heap property on them by perhaps swapping the parent with the
+	 * lesser of the two children. The child thus affected must be heapified itself
+	 * by a recursive call.
+	 * 
 	 * @param where the index into the array where the parent lives
 	 */
 	private void heapify(int where) {
 		//
 		// As described in lecture
-		//  FIXME
+		// FIXME
 		//
-		Decreaser<T> x = array[where];
-		if ((!(array[2*where].loc>size))&&((array[2*where]!=null))) {
-		ticker.tick(1);
-		if ((array[2*where+1]==null)&&(array[2*where+1].loc>size)&&((array[2*where].getValue().compareTo(array[where].getValue()))<0)) {
-		array[where]=array[2*where];
-		array[2*where]=x;
-		array[where].loc=where;
-		array[2*where].loc=2*where;
-		ticker.tick(5);
-		}else if ((!(array[2*where+1].loc>size))&&((array[2*where].getValue().compareTo(array[2*where+1].getValue()))<0)&&((array[2*where].getValue().compareTo(array[where].getValue()))<0)) {
-		array[where]=array[2*where];
-		array[2*where]=x;
-		array[where].loc=where;
-		array[2*where].loc=2*where;
-		heapify(array[2*where].loc);
-		ticker.tick(6);
-		}else if((!(array[2*where+1].loc>size))&&(!((array[2*where].getValue().compareTo(array[2*where+1].getValue()))<0))&&((array[2*where+1].getValue().compareTo(array[where].getValue()))<0)) {
-		array[where]=array[2*where+1];
-		array[2*where+1]=x;
-		array[where].loc=where;
-		array[2*where+1].loc=2*where+1;
-		heapify(array[2*where+1].loc);
-		ticker.tick(6);
+		if (isEmpty() || size == 1) {
+			ticker.tick();
+			return;
 		}
-		}else {
-		ticker.tick(2);
-		return;
+		if (2 * where + 1 <= size) {
+			if (array[2 * where].getValue().compareTo(array[where].getValue()) > 0
+					&& array[2 * where + 1].getValue().compareTo(array[where].getValue()) > 0) {
+				ticker.tick();
+				return;
+			}
+			if (array[2 * where + 1].getValue().compareTo(array[2 * where].getValue()) > 0) {
+				Decreaser<T> store = array[2 * where];
+				array[2 * where] = array[where];
+				array[where] = store;
+				array[where].loc = where;
+				array[2 * where].loc = 2 * where;
+				heapify(2 * where);
+				ticker.tick(6);
+			} else {
+				Decreaser<T> store = array[2 * where + 1];
+				array[2 * where + 1] = array[where];
+				array[where] = store;
+				array[where].loc = where;
+				array[2 * where + 1].loc = 2 * where + 1;
+				heapify(2 * where + 1);
+				ticker.tick(6);
+			}
+		} else if (2 * where <= size) {
+			if (array[2 * where].getValue().compareTo(array[where].getValue()) > 0) {
+				ticker.tick();
+				return;
+			} else if (array[2 * where].getValue().compareTo(array[where].getValue()) < 0) {
+				Decreaser<T> store = array[2 * where];
+				array[2 * where] = array[where];
+				array[where] = store;
+				array[where].loc = where;
+				array[2 * where].loc = 2 * where;
+
+				heapify(2 * where);
+				ticker.tick(6);
+			}
 		}
-		}
-		
-			
-		
-	
-	
+		ticker.tick();
+
+	}
+
 	/**
-	 * Does the heap contain anything currently?
-	 * I implemented this for you.  Really, no need to thank me!
+	 * Does the heap contain anything currently? I implemented this for you. Really,
+	 * no need to thank me!
 	 */
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	
+
 	//
 	// End of methods described in lecture
 	//
-	
+
 	//
 	// The methods that follow are necessary for the debugging
-	//   infrastructure.
+	// infrastructure.
 	//
 	/**
-	 * This method would normally not be present, but it allows
-	 *   our consistency checkers to see if your heap is in good shape.
+	 * This method would normally not be present, but it allows our consistency
+	 * checkers to see if your heap is in good shape.
+	 * 
 	 * @param loc the location
 	 * @return the value currently stored at the location
 	 */
 	public T peek(int loc) {
 		if (array[loc] == null)
 			return null;
-		else return array[loc].getValue();
+		else
+			return array[loc].getValue();
 	}
 
 	/**
-	 * Return the loc information from the Decreaser stored at loc.  They
-	 *   should agree.  This method is used by the heap validator.
+	 * Return the loc information from the Decreaser stored at loc. They should
+	 * agree. This method is used by the heap validator.
+	 * 
 	 * @param loc
 	 * @return the Decreaser's view of where it is stored
 	 */
@@ -238,16 +247,14 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 	public int size() {
 		return this.size;
 	}
-	
+
 	public int capacity() {
-		return this.array.length-1;
+		return this.array.length - 1;
 	}
-	
 
 	/**
-	 * The commented out code shows you the contents of the array,
-	 *   but the call to HeapToStrings.toTree(this) makes a much nicer
-	 *   output.
+	 * The commented out code shows you the contents of the array, but the call to
+	 * HeapToStrings.toTree(this) makes a much nicer output.
 	 */
 	public String toString() {
 //		String ans = "";
@@ -259,22 +266,24 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 	}
 
 	/**
-	 * This is not the unit test, but you can run this as a Java Application
-	 * and it will insert and extract 100 elements into the heap, printing
-	 * the heap each time it inserts.
+	 * This is not the unit test, but you can run this as a Java Application and it
+	 * will insert and extract 100 elements into the heap, printing the heap each
+	 * time it inserts.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "You are welcome to run this, but be sure also to run the TestMinHeap JUnit test");
+		JOptionPane.showMessageDialog(null,
+				"You are welcome to run this, but be sure also to run the TestMinHeap JUnit test");
 		MinHeap<Integer> h = new MinHeap<Integer>(500, new Ticker());
 		MinHeapValidator<Integer> v = new MinHeapValidator<Integer>(h);
 		Random r = new Random();
-		for (int i=0; i < 100; ++i) {
+		for (int i = 0; i < 100; ++i) {
 			v.check();
 			h.insert(r.nextInt(1000));
 			v.check();
 			System.out.println(HeapToStrings.toTree(h));
-			//System.out.println("heap is " + h);
+			// System.out.println("heap is " + h);
 		}
 		while (!h.isEmpty()) {
 			int next = h.extractMin();
@@ -282,5 +291,5 @@ public class MinHeap<T extends Comparable<T>> implements PriorityQueue<T> {
 		}
 	}
 
-
 }
+
